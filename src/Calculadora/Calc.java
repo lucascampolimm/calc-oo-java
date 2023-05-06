6a23bbc0911d7b5aba1bea3736c51301cf1dae9d
Lucas Campolim <lucascampolim@protonmail.com>
2023-05-06 09:35:41 -0400

    improvements

--- src/Calculadora/Calc.java
+++ src/Calculadora/Calc.java
@@ -4,26 +4,27 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Calc {
-	
+
 	public int somar(int num1, int num2) {
 		return num1 + num2;
 	}
-	
+
 	public int diminuir(int num1, int num2) {
 		return num1 - num2;
 	}
-	
+
 	public int multiplicar(int num1, int num2) {
 		return num1 * num2;
 	}
+
 	public double dividir(double num1, double num2) {
 		return num1 / num2;
 	}
-	
+
 	public int resto(int num1, int num2) {
 		return num1 % num2;
 	}
-	
+
 	public void menu() {
 		System.out.println("\nEscolha uma opção: \n");
 		System.out.println("1) Somar ");
@@ -34,15 +35,15 @@ public class Calc {
 		System.out.println("6) Sair \n");
 		System.out.print(">> ");
 	}
-	
+
 	@SuppressWarnings("deprecation")
-	public static void main(String[] args) throws IOException, InterruptedException {	
-		
+	public static void main(String[] args) throws IOException, InterruptedException {
+
 		Calc c = new Calc();
 		int num1 = 0, num2 = 0, sair = 0, op;
 		Scanner input = new Scanner(System.in);
-		
-		while(sair == 0) {
+
+		while (sair == 0) {
 			c.menu();
 			op = input.nextInt();
 			if (op >= 1 && op <= 5) {
@@ -51,41 +52,41 @@ public class Calc {
 				System.out.print("Digite o segundo número: ");
 				num2 = input.nextInt();
 			}
-			
-			switch(op) {
-				case 1:	
-					System.out.print("O resultado da soma é: " + c.somar(num1, num2));
-					break;
-				case 2:
-					System.out.print("O resultado da subtração é: " + c.diminuir(num1, num2));
-					break;
-				case 3:
-					System.out.print("O resultado da multiplicação é: " + c.multiplicar(num1, num2));
-					break;
-				case 4:
-					if (num2 == 0) {
-						System.out.print("Não é possível dividir por zero!");
-					} else {
-						System.out.printf("O resultado da divisão é: %.2f", c.dividir((double)num1, (double)num2));
-					}
-					break;
-				case 5:
-					System.out.print("O resultado do resto da divisão inteira é: " + c.resto(num1, num2));
-					break;
-				case 6:
-					sair = 1;
-					input.close();
-					System.out.print("\nVocê saiu do programa!");
-					return;
-				default:
-					System.out.print("\nOpção inválida!");
+
+			switch (op) {
+			case 1:
+				System.out.print("O resultado da soma é: " + c.somar(num1, num2));
+				break;
+			case 2:
+				System.out.print("O resultado da subtração é: " + c.diminuir(num1, num2));
+				break;
+			case 3:
+				System.out.print("O resultado da multiplicação é: " + c.multiplicar(num1, num2));
+				break;
+			case 4:
+				if (num2 == 0) {
+					System.out.print("Não é possível dividir por zero!");
+				} else {
+					System.out.printf("O resultado da divisão é: %.2f", c.dividir((double) num1, (double) num2));
+				}
+				break;
+			case 5:
+				System.out.print("O resultado do resto da divisão inteira é: " + c.resto(num1, num2));
+				break;
+			case 6:
+				sair = 1;
+				input.close();
+				System.out.print("\nVocê saiu do programa!");
+				return;
+			default:
+				System.out.print("\nOpção inválida!");
 			}
 			System.out.print("\n\nPrecione qualquer tecla para continuar...");
 			System.in.read();
 			if (System.getProperty("os.name").contains("Windows"))
 				new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-	        else
-	        	Runtime.getRuntime().exec("clear");
+			else
+				Runtime.getRuntime().exec("clear");
 		}
 		input.close();
 	}
